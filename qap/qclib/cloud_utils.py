c120dcfa33279c6b9c7e94ac9992fd27b071f135
sgiavasis <sgiava77@gmail.com>
2015-06-05 16:47:13 -0400

    Made changes so that the S3 filepath download only runs when necessary.

--- qap/qclib/cloud_utils.py
+++ qap/qclib/cloud_utils.py
@@ -5,26 +5,39 @@
 '''
 '''
 
-# Function to build, select, and download subject data
-def dl_subj_from_s3(subj_idx, img_type, creds_path):
-    '''
-    '''
+def pull_S3_sublist(yaml_outpath, img_type, cfg_file):
 
-    # Import packages
-    from CPAC.AWS import fetch_creds, aws_utils
+    # function example use:
+    #
+    # yamlpath = os.path.join(os.getcwd(), "s3dict.yml")
+    #
+    # # Build entire filepath dictionary from S3
+    # s3_dict_yml = pull_S3_sublist(yamlpath, 'anat', args.config)
 
-    # Init variables
-    bucket_prefix = 'data/Projects/ABIDE_Initiative/RawData'
-    local_prefix ='/mnt/inputs'
-    bucket = fetch_creds.return_bucket(creds_path, 'fcp-indi')
+    import os
+    from CPAC.AWS import fetch_creds
+    import yaml
+
     s3_list = []
     s3_dict = {}
+
+    # Load config file
+    with open(cfg_file,'r') as f:
+        cfg_dict = yaml.load(f)
+
+    bucket_name = cfg_dict["bucket_name"]
+    bucket_prefix = cfg_dict["bucket_prefix"]
+    creds_path = cfg_dict["creds_path"]
+
+    bucket = fetch_creds.return_bucket(creds_path, bucket_name)
+
 
     # Filter for anat/rest
     if img_type == 'anat':
         subkey_type = 'anatomical_scan'
     elif img_type == 'rest':
         subkey_type = 'functional_scan'
+
 
     # Build S3-subjects to download
     for bk in bucket.list(prefix=bucket_prefix):
@@ -59,11 +72,63 @@ def dl_subj_from_s3(subj_idx, img_type, creds_path):
         else:
 
             continue
+    
+            
+    if len(s3_dict) == 0:
+        err = "\n[!] Filepaths have not been successfully gathered from " \
+              "the S3 bucket!\n"
+        raise Exception(err)
+            
+           
+    # write yaml file
+    with open(yaml_outpath,"wt") as f:
+        f.write(yaml.dump(s3_dict))
+        
+    
+    if os.path.isfile(yaml_outpath):
+        return yaml_outpath
+    else:
+        err = "\n[!] Filepaths from the S3 bucket have not been " \
+              "successfully saved to the YAML file!\nOutput filepath: %s\n" \
+              % yaml_outpath
+        raise Exception(err)
 
+
+
+# Function to build, select, and download subject data
+def dl_subj_from_s3(subj_idx, cfg_file, s3_dict_yaml):
+    '''
+    '''
+
+    # Import packages
+    from CPAC.AWS import fetch_creds, aws_utils
+    import yaml
+
+    # Load config file
+    with open(cfg_file,'r') as f:
+        cfg_dict = yaml.load(f)
+
+    # Init variables
+    bucket_prefix = cfg_dict["bucket_prefix"]
+    local_prefix = cfg_dict["local_prefix"]
+    bucket_name = cfg_dict["bucket_name"]
+    creds_path = cfg_dict["creds_path"]
+    
+    bucket = fetch_creds.return_bucket(creds_path, bucket_name)
+
+    s3_list = []
+    s3_dict = {}
+
+
+    # pull in S3 dict yaml
+    with open(s3_dict_yaml,'r') as f:
+        s3_dict = yaml.load(f)
 
     if len(s3_dict) == 0:
-        err = "\n[!] Filepaths have not been successfully gathered from the S3 bucket!\n"
+        err = "\n[!] Filepaths have not been successfully gathered from " \
+              "the filepath YAML dictionary!\n"
         raise Exception(err)
+
 
     # Get list of subject keys for indexing
     sd_keys = s3_dict.keys()
@@ -72,14 +137,15 @@ def dl_subj_from_s3(subj_idx, img_type, creds_path):
     # Grab subject dictionary of interest
     subj_key = sd_keys[subj_idx-1]
     sub_dict = s3_dict[subj_key]
-    print "sub_dict: ", sub_dict
+
     # Download subject data to local prefix
     s3_dl = []
     for s3_key, s3_path in sub_dict.items():
         s3_dl.append(s3_path)
         sub_dict[s3_key] = s3_path.replace(bucket_prefix, local_prefix)
 
-    aws_utils.s3_download(bucket, s3_dl, local_prefix=local_prefix, bucket_prefix=bucket_prefix)
+    aws_utils.s3_download(bucket, s3_dl, local_prefix=local_prefix, \
+                              bucket_prefix=bucket_prefix)
 
     sub_dict = {subj_key : sub_dict}
 
@@ -87,7 +153,8 @@ def dl_subj_from_s3(subj_idx, img_type, creds_path):
     return sub_dict
 
 
-def upl_qap_output(cfg_file, creds_path):
+
+def upl_qap_output(cfg_file):
     '''
     '''
 
@@ -96,11 +163,17 @@ def upl_qap_output(cfg_file, creds_path):
     import os
     import yaml
 
-    # Init variables
-    bucket = fetch_creds.return_bucket(creds_path, 'fcp-indi')
-
     # Load config file
-    cfg_dict = yaml.load(open(cfg_file, 'r'))
+    with open(cfg_file,'r') as f:
+        cfg_dict = yaml.load(f)
+
+    # Init variables
+    bucket_name = cfg_dict["bucket_name"]
+    bucket_out_prefix = cfg_dict["bucket_out_prefix"]
+    creds_path = cfg_dict["creds_path"]
+    
+    bucket = fetch_creds.return_bucket(creds_path, bucket_name)
+        
     output_dir = cfg_dict['output_directory']
 
     # And upload data
@@ -110,7 +183,9 @@ def upl_qap_output(cfg_file, creds_path):
             upl_files.extend([os.path.join(root, fil) for fil in files])
 
     # Using CPAC AWS utils
-    s3_upl_files = [ufile.replace(output_dir, 'data/Projects/ABIDE_Initiative/Outputs/qap') \
+    s3_upl_files = [ufile.replace(output_dir, bucket_out_prefix) \
                    for ufile in upl_files]
     aws_utils.s3_upload(bucket, upl_files, s3_upl_files)
+
+
 
