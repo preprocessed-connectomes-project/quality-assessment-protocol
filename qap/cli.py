efb2cd97265d51992b21111e87b4aa35b670996d
Oscar Esteban <code@oscaresteban.es>
2015-11-13 07:59:06 +0100

    fix headers

--- qap/cli.py
+++ qap/cli.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date:   2015-10-16 12:52:35
-# @Last Modified by:   Oscar Esteban
-# @Last Modified time: 2015-10-22 12:16:13
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
 import os
 import os.path as op
 import time
