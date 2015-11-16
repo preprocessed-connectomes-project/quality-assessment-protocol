4e110ea96ca64458083281de62212b0fee16c5c6
John Pellman <pellman.john@gmail.com>
2015-11-16 13:12:14 -0500

    See if jekyll will build

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
-ls -l $HOME/.ssh
-#cd ./docs
-#jekyll build
-#mv ./_site ~/site
-#cd ~ && git clone -b gh-pages ${CIRCLE_PROJECT_REPONAME} currentsite
+cd ./docs
+jekyll build
+mv ./_site ~/site
 cd ~ && git clone -b gh-pages_test https://github.com/preprocessed-connectomes-project/quality-assessment-protocol.git currentsite
-#mv ~/site/* ~/currentsite && cd ~/currentsite
-cd ~/currentsite
-touch ${CIRCLE_SHA1}
+mv ~/site/* ~/currentsite && cd ~/currentsite
 git add -A
 git commit -m "Automatic documentation update for build "${CIRCLE_SHA1}
 git push
