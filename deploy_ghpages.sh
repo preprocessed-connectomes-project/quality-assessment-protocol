83e2ff3f6a4a9978ec2abdac5023217caa242be2
John Pellman <pellman.john@gmail.com>
2015-11-16 11:05:29 -0500

    typo

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -5,7 +5,7 @@
 #cd ~ && git clone -b gh-pages ${CIRCLE_PROJECT_REPONAME} currentsite
 cd ~ && git clone -b gh-pages_test https://github.com/preprocessed-connectomes-project/quality-assessment-protocol.git currentsite
 #mv ~/site/* ~/currentsite && cd ~/currentsite
-cd ~/currensite
+cd ~/currentsite
 touch ${CIRCLE_SHA1}
 git add -A
 git commit -m "Automatic documentation update for build "${CIRCLE_SHA1}
