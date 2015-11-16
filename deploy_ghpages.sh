32c9ab85b04532ab1206dbdc903c6d7ce4c7a3a8
John Pellman <pellman.john@gmail.com>
2015-11-16 09:55:06 -0500

    Testing git push

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-cd ./docs
-jekyll build
-mv ./_site ~/site
+#cd ./docs
+#jekyll build
+#mv ./_site ~/site
 #cd ~ && git clone -b gh-pages ${CIRCLE_PROJECT_REPONAME} currentsite
 cd ~ && git clone -b gh-pages_test https://github.com/preprocessed-connectomes-project/quality-assessment-protocol.git currentsite
-mv ~/site/* ~/currentsite && cd ~/currentsite
+#mv ~/site/* ~/currentsite && cd ~/currentsite
+git touch ~/currentsite/${CIRCLE_SHA1}
 git add -A
 git commit -m "Automatic documentation update for build "${CIRCLE_SHA1}
 git push
