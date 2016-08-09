2c01b33091a1c74212341cbffa960aae3da08f68
Cameron Craddock <cameron.craddock@gmail.com>
2016-08-09 00:01:43 -0400

    fixed to handle sessions and to construct scan key name from acq and task keys

--- scripts/qap_bids_data_sublist_generator.py
+++ scripts/qap_bids_data_sublist_generator.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
-def gather_bids_data(dataset_folder, yaml_outpath, subject_inclusion=None,
+def gather_bids_data(dataset_folder, subject_inclusion=None,
                      scan_type=None):
 
     import os
     import os.path as op
-    import yaml
     from glob import glob
 
     sub_dict = {}
     inclusion_list = []
 
     subject_ids = [x for x in next(os.walk(dataset_folder))[1] if x.startswith("sub-")]
-
     if scan_type is None:
         scan_type = 'functional anatomical'
 
@@ -33,34 +31,69 @@ def gather_bids_data(dataset_folder, yaml_outpath, subject_inclusion=None,
 
     for subject_id in subject_ids:
         # TODO: implement multisession support
-        session_name = 'session_1'
+        sessions = [x for x in next(os.walk(op.join(dataset_folder,subject_id)))[1] if x.startswith("ses-")]
 
-        anatomical_scans = sorted(glob(op.join(
-            dataset_folder, subject_id, "anat",
-            "%s_*T1w.nii.gz" % subject_id, )))
+        if not sessions:
+            sessions = ['']
 
-        functional_scans = sorted(glob(op.join(
-            dataset_folder, subject_id, "func",
-            "%s_*bold.nii.gz" % subject_id, )))
+        for session_dir in sessions:
 
-        if anatomical_scans or functional_scans:
-            sub_dict[subject_id] = {session_name: {}}
-            if anatomical_scans and get_anat:
-                sub_dict[subject_id][session_name]["anatomical_scan"] = {}
-                for i, anatomical_scan in enumerate(anatomical_scans):
-                    sub_dict[subject_id][session_name]["anatomical_scan"]["anat_%d"%(i+1)] = op.abspath(anatomical_scan)
-            if functional_scans and get_func:
-                sub_dict[subject_id][session_name]["functional_scan"] = {}
-                for i, anatomical_scan in enumerate(functional_scans):
-                    sub_dict[subject_id][session_name]["functional_scan"]["func_%d"%(i+1)] = op.abspath(anatomical_scan)
+            # the session name is just the session directory name
+            session_name = session_dir
 
-    with open(yaml_outpath, "wt") as f:
-        f.write(yaml.dump(sub_dict))
+            # unless the session directory is missing, in which
+            # case set the session_name to ses-1
+            if not session_name:
+                session_name = "ses-1"
+
+            # for now restrict our analysis to T1 weighted anats and
+            # BOLD weighted functionals
+            anatomical_scans = sorted(glob(op.join(
+                dataset_folder,  subject_id, session_dir, "anat",
+                "%s_*T1w.nii.gz" % subject_id, )))
+
+            functional_scans = sorted(glob(op.join(
+                dataset_folder, subject_id, session_dir, "func",
+                "%s_*bold.nii.gz" % subject_id, )))
+
+            if anatomical_scans or functional_scans:
+                sub_dict[subject_id] = {session_name: {}}
+                if anatomical_scans and get_anat:
+                    sub_dict[subject_id][session_name]["anatomical_scan"] = {}
+                    for i, anatomical_scan in enumerate(anatomical_scans):
+                        sd={s.split("-")[0]:s.split("-")[1] \
+                            for s in anatomical_scan.split("_") \
+                            if len(s.split("-")) > 1}
+                        anat_key=(anatomical_scan.split("_")[-1]).split(".")[0]
+                        if "acq" in sd.keys():
+                            anat_key = "_".join([anat_key,sd["acq"]])
+                        sub_dict[subject_id][session_name]["anatomical_scan"][anat_key] = op.abspath(anatomical_scan)
+
+                if functional_scans and get_func:
+                    sub_dict[subject_id][session_name]["functional_scan"] = {}
+                    for i, functional_scan in enumerate(functional_scans):
+                        sd={s.split("-")[0]:s.split("-")[1] \
+                            for s in functional_scan.split("_") \
+                            if len(s.split("-")) > 1}
+                        func_key=(functional_scan.split("_")[-1]).split(".")[0]
+                        if "task" in sd.keys():
+                            func_key="_".join([func_key,sd["task"]])
+                        else:
+                            raise Exception("%s: Missing task key , this does"\
+                                "not appear to be a BIDS dataset."\
+                                %(functional_scan))
+                        if "acq" in sd.keys():
+                            func_key = "_".join([func_key,sd["acq"]])
+                        sub_dict[subject_id][session_name]["functional_scan"][func_key] = op.abspath(anatomical_scan)
+
+    return(sub_dict)
+
 
 
 def main():
 
     import argparse
+    import yaml
 
     parser = argparse.ArgumentParser()
 
@@ -84,9 +117,11 @@ def main():
     args = parser.parse_args()
 
     # run it!
-    gather_bids_data(args.dataset_folder, args.outfile_path, args.include,
+    sub_dict = gather_bids_data(args.dataset_folder, args.include,
                      args.type)
 
+    with open(args.outfile_path, "wt") as f:
+        f.write(yaml.dump(sub_dict))
 
 if __name__ == "__main__":
     main()
