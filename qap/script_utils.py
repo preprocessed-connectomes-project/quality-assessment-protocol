779c194119638213699e5fa7bb3566173fb8b06e
sgiavasis <sgiava77@gmail.com>
2016-09-12 17:07:14 -0400

    Moved more script functions to script_utils

--- qap/script_utils.py
+++ qap/script_utils.py
@@ -111,6 +111,116 @@ def gather_raw_data(site_folder, include_sites=False, subject_inclusion=None):
     return sub_dict
 
 
+def populate_custom_data_dict(data_dict, filepath, part_id, session_id, series_id,
+    site_id, resource, default_series_label=None):
+
+    new_dict = data_dict
+
+    if not session_id:
+        session_id = "session_1"
+    if not series_id:
+        if default_series_label:
+            series_id = default_series_label
+        else:
+            series_id = "scan_1"
+
+    if part_id not in new_dict.keys():
+        new_dict[part_id] = {}
+    if session_id not in new_dict[part_id].keys():
+        new_dict[part_id][session_id] = {}
+    if resource not in new_dict[part_id][session_id].keys():
+        new_dict[part_id][session_id][resource] = {}
+    if site_id not in new_dict[part_id][session_id].keys():
+        new_dict[part_id][session_id]["site_name"] = site_id
+    if series_id not in new_dict[part_id][session_id][resource].keys():
+        new_dict[part_id][session_id][resource][series_id] = filepath    
+
+    data_dict.update(new_dict)
+
+    return data_dict
+
+
+
+def gather_custom_raw_data(base_folder, directory_format, anatomical_keywords=None,
+    functional_keywords=None):
+
+    import os
+    from qap.script_utils import populate_custom_data_dict
+
+    if "{participant}" not in directory_format:
+        pass
+
+    data_dict = {}
+
+    format_list = [x for x in directory_format.split("/") if x != ""]
+
+    indices = {}
+    operators = ["{site}", "{participant}", "{session}", "{series}"]
+
+    for op in operators:
+        if op in format_list:
+            indices[op] = format_list.index(op)
+
+    if anatomical_keywords:
+        anatomical_keywords = [x for x in anatomical_keywords.split(" ") if x != ""]
+
+    if functional_keywords:
+        functional_keywords = [x for x in functional_keywords.split(" ") if x != ""]
+
+    for root, dirs, files in os.walk(base_folder):
+        for filename in files:
+            if ".nii" in filename:
+
+                filepath = os.path.join(root, filename)
+
+                second_half_list = [x for x in filepath.split(base_folder)[1].split("/") if x != ""]
+
+                site_id = None
+                part_id = None
+                session_id = None
+                series_id = None
+
+                if "{site}" in indices.keys():
+                    site_id = second_half_list[indices["{site}"]]
+                if "{participant}" in indices.keys():
+                    part_id = second_half_list[indices["{participant}"]]
+                if "{session}" in indices.keys():
+                    session_id = second_half_list[indices["{session}"]]
+                if "{series}" in indices.keys():
+                    series_id = second_half_list[indices["{series}"]]
+
+                if anatomical_keywords:
+                    for word in anatomical_keywords:
+                        if (word in filename) or (word in session_id):
+
+                            data_dict = populate_custom_data_dict(data_dict,
+                                                           filepath, part_id,
+                                                           session_id,
+                                                           series_id,
+                                                           site_id,
+                                                           "anatomical_scan",
+                                                           "anat_1")
+
+                if functional_keywords:
+                    for word in functional_keywords:
+                        if (word in filename) or (word in session_id):
+
+                            data_dict = populate_custom_data_dict(data_dict,
+                                                           filepath, part_id,
+                                                           session_id,
+                                                           series_id,
+                                                           site_id,
+                                                           "functional_scan",
+                                                           "func_1")
+
+    if len(data_dict) == 0:
+        err = "\n\n[!] No data files were found given the inputs you " \
+              "provided! Double-check your setup.\n\n"
+        raise Exception(err)
+
+    return data_dict
+
+
 def pull_S3_sublist(bucket_name, bucket_prefix, creds_path):
 
     import os
@@ -282,6 +392,180 @@ def create_subdict_from_s3_list(s3_list, bucket_prefix, session_list=None,
     return s3_dict
 
 
+def create_CPAC_outputs_dict(cpac_outdir, qap_type, session_format):
+
+    import os
+    import glob
+
+    if qap_type == "anat":
+
+        outputs = ["anatomical_reorient", "anatomical_csf_mask", \
+                   "anatomical_gm_mask", "anatomical_wm_mask", \
+                   "anatomical_to_mni_linear_xfm"]
+
+    elif qap_type == "func":
+
+        outputs = ["mean_functional", "functional_brain_mask", \
+                   "raw_functional", "coordinate_transformation"]
+
+    outputs_dict = {}
+
+    for sub_dir in os.listdir(cpac_outdir):
+
+        if not os.path.isdir(os.path.join(cpac_outdir, sub_dir)):
+            continue
+
+        sessions = []
+
+        # if the folder structure is sub_id/session_id/scan_id/...
+        if session_format == 1:
+            for session in os.listdir(os.path.join(cpac_outdir, sub_dir)):
+                if os.path.isdir(os.path.join(cpac_outdir, sub_dir, session)):
+                    sessions.append(session)
+
+        # if there is no session label in the folder structure
+        if session_format == 2:
+            # since there is no session, let's assign one
+            sessions = ["session_1"]
+
+        # if the session is embedded in the subject ID
+        if session_format == 3:
+            subject_session = sub_dir
+
+            if "_" not in sub_dir:
+                err = "\n\n[!] You said your CPAC output directory had the " \
+                      "session IDs embedded in the subject IDs, but it " \
+                      "doesn't seem that way for subject ID %s!\n\nIs it " \
+                      " formatted like this?   ../pipeline_output/subject_" \
+                      "session/output/..\n\nIf not, you're using the wrong " \
+                      "option for session_format! Use the -h flag to see " \
+                      "the documentation.\n\n%s not being included in the " \
+                      "subject list.\n\n" % (sub_dir, sub_dir)
+                print err
+                continue
+
+            session_id = sub_dir.split("_",1)[1]
+            sub_dir = sub_dir.split("_",1)[0]
+            sessions = [session_id]
+
+        for session in sessions:
+
+            for resource in outputs:
+
+                resource_path = ""
+
+                if session_format == 1:
+                    resource_folder = os.path.join(cpac_outdir, sub_dir, \
+                                                       session, resource)
+                elif session_format == 2:
+                    resource_folder = os.path.join(cpac_outdir, sub_dir, \
+                                                       resource)
+
+                elif session_format == 3:
+                    resource_folder = os.path.join(cpac_outdir, \
+                                                       subject_session, \
+                                                       resource)
+
+                # if this current resource/output does not exist for this
+                # subject, go to next resource in list
+                if not os.path.isdir(resource_folder):
+                    continue
+
+                if qap_type == "anat":
+
+                    ''' until CPAC writes multiple anat scans in the '''
+                    ''' output folder structure '''
+                    scans = ["anat_1"]
+
+                if qap_type == "func":
+    
+                    scans = []
+
+                    for item in os.listdir(resource_folder):
+                        if os.path.isdir(os.path.join(resource_folder, item)):
+                            item = item.replace("_scan_","")
+                            item = item.replace("_rest","")
+                            scans.append(item)
+
+                for scan in scans:
+
+                    if qap_type == "anat":
+
+                        if "mask" in resource:
+                            resource_paths = glob.glob(os.path.join(resource_folder, "*", "*"))
+                        else:
+                            resource_paths = glob.glob(os.path.join(resource_folder, "*"))
+
+                        if len(resource_paths) == 1:
+                            resource_path = resource_paths[0]
+                        else:
+                            print "\nMultiple files for %s for subject %s!!" \
+                                  % (resource, sub_dir)
+                            print "Check the directory: %s" \
+                                      % resource_folder
+                            print "%s for %s has not been included in the " \
+                                  "subject list.\n" % (resource, sub_dir)
+                            continue
+
+                    if qap_type == "func":
+
+                        fullscan = "_scan_" + scan + "_rest"
+
+                        resource_paths = glob.glob(os.path.join(resource_folder, fullscan, "*"))
+
+                        if len(resource_paths) == 1:
+                            resource_path = resource_paths[0]
+                        else:
+                            print "\nMultiple files for %s for subject %s!!" \
+                                  % (resource, sub_dir)
+                            print "Check the directory: %s" \
+                                      % resource_folder
+                            print "%s for %s has not been included in the " \
+                                  "subject list.\n" % (resource, sub_dir)
+                            continue
+
+                    ''' put a catch here for multiple files '''
+
+                    if sub_dir not in outputs_dict.keys():
+                        outputs_dict[sub_dir] = {}
+
+                    if session not in outputs_dict[sub_dir].keys():
+                        outputs_dict[sub_dir][session] = {}
+
+                    if resource not in outputs_dict[sub_dir][session].keys():
+                        outputs_dict[sub_dir][session][resource] = {}
+
+                    if scan not in outputs_dict[sub_dir][session][resource].keys():
+                        outputs_dict[sub_dir][session][resource][scan] = resource_path
+
+    # make up for QAP - CPAC resource naming discrepancy
+    for subid in outputs_dict.keys():
+
+        for session in outputs_dict[subid].keys():
+
+            for resource in outputs_dict[subid][session].keys():
+
+                if resource == "motion_correct":
+
+                    filepath = outputs_dict[subid][session]["motion_correct"]
+
+                    outputs_dict[subid][session]["func_motion_correct"] = \
+                        filepath
+
+                    del outputs_dict[subid][session]["motion_correct"]
+
+                if resource == "anatomical_to_mni_linear_xfm":
+
+                    filepath = outputs_dict[subid][session]["anatomical_to_mni_linear_xfm"]
+
+                    outputs_dict[subid][session]["flirt_affine_xfm"] = \
+                        filepath
+
+                    del outputs_dict[subid][session]["anatomical_to_mni_linear_xfm"]
+
+    return outputs_dict
+
+
 def write_inputs_dict_to_yaml_file(input_dict, yaml_outpath):
 
     import os
@@ -294,7 +578,7 @@ def write_inputs_dict_to_yaml_file(input_dict, yaml_outpath):
     # write yaml file
     try:
         with open(yaml_outpath,"wt") as f:
-            f.write(yaml.dump(input_dict))
+            f.write(yaml.dump(input_dict, default_flow_style=True))
     except:
         err = "\n\n[!] Error writing YAML file output.\n1. Do you have " \
               "write permissions for the output path provided?\n2. Did you " \
--- scripts/qap_flexible_sublist_generator.py
+++ scripts/qap_flexible_sublist_generator.py
@@ -1,130 +1,9 @@
 #!/user/bin/env python
 
-def populate_data_dict(data_dict, filepath, part_id, session_id, series_id,
-	site_id, resource, default_series_label=None):
-
-    new_dict = data_dict
-
-    if not session_id:
-        session_id = "session_1"
-    if not series_id:
-    	if default_series_label:
-    		series_id = default_series_label
-    	else:
-    		series_id = "scan_1"
-
-    if part_id not in new_dict.keys():
-        new_dict[part_id] = {}
-    if session_id not in new_dict[part_id].keys():
-        new_dict[part_id][session_id] = {}
-    if resource not in new_dict[part_id][session_id].keys():
-        new_dict[part_id][session_id][resource] = {}
-    if site_id not in new_dict[part_id][session_id].keys():
-        new_dict[part_id][session_id]["site_name"] = site_id
-    if series_id not in new_dict[part_id][session_id][resource].keys():
-        new_dict[part_id][session_id][resource][series_id] = filepath    
-
-    data_dict.update(new_dict)
-
-    return data_dict
-
-
-
-def gather_raw_data(base_folder, directory_format, anatomical_keywords=None,
-	functional_keywords=None):
-
-    import os
-
-    if "{participant}" not in directory_format:
-    	pass
-
-    data_dict = {}
-
-    format_list = [x for x in directory_format.split("/") if x != ""]
-
-    indices = {}
-    operators = ["{site}", "{participant}", "{session}", "{series}"]
-
-    for op in operators:
-    	if op in format_list:
-    		indices[op] = format_list.index(op)
-
-    if anatomical_keywords:
-        anatomical_keywords = [x for x in anatomical_keywords.split(" ") if x != ""]
-
-    if functional_keywords:
-        functional_keywords = [x for x in functional_keywords.split(" ") if x != ""]
-
-    for root, dirs, files in os.walk(base_folder):
-        for filename in files:
-            if ".nii" in filename:
-
-                filepath = os.path.join(root, filename)
-
-                second_half_list = [x for x in filepath.split(base_folder)[1].split("/") if x != ""]
-
-                site_id = None
-                part_id = None
-                session_id = None
-                series_id = None
-
-                if "{site}" in indices.keys():
-                	site_id = second_half_list[indices["{site}"]]
-                if "{participant}" in indices.keys():
-                	part_id = second_half_list[indices["{participant}"]]
-                if "{session}" in indices.keys():
-                	session_id = second_half_list[indices["{session}"]]
-                if "{series}" in indices.keys():
-                	series_id = second_half_list[indices["{series}"]]
-
-                if anatomical_keywords:
-                    for word in anatomical_keywords:
-                        if (word in filename) or (word in session_id):
-
-                            data_dict = populate_data_dict(data_dict,
-                                                           filepath, part_id,
-                                                           session_id,
-                            	                           series_id,
-                                                           site_id,
-                                                           "anatomical_scan",
-                                                           "anat_1")
-
-                if functional_keywords:
-                    for word in functional_keywords:
-                        if (word in filename) or (word in session_id):
-
-                            data_dict = populate_data_dict(data_dict,
-                                                           filepath, part_id,
-                                                           session_id,
-                                                           series_id,
-                                                           site_id,
-                                                           "functional_scan",
-                                                           "func_1")
-
-    if len(data_dict) == 0:
-    	err = "\n\n[!] No data files were found given the inputs you " \
-    	      "provided! Double-check your setup.\n\n"
-    	raise Exception(err)
-
-    return data_dict
-
-
-
-def write_yaml_file(data_dict, out_file):
-
-    import yaml
-
-    if "." in out_file:
-        out_file = out_file.split(".")[0]
-
-    out_file = out_file + ".yml"
-
-    with open(out_file,"wt") as f:
-        f.write(yaml.dump(data_dict))
-
-
-
 def main():
+
+    from qap.script_utils import gather_custom_raw_data, \
+                                 write_inputs_dict_to_yaml_file
 
     import argparse
 
@@ -160,11 +39,11 @@ def main():
     args = parser.parse_args()
 
     # run the thing
-    data_dict = gather_raw_data(args.base_directory, args.directory_format,
-        args.anatomical_keywords, args.functional_keywords)
+    data_dict = gather_custom_raw_data(args.base_directory, 
+        args.directory_format, args.anatomical_keywords, 
+        args.functional_keywords)
 
-    write_yaml_file(data_dict, args.outfile_path)
-
+    write_inputs_dict_to_yaml_file(data_dict, args.outfile_path)
 
 
 if __name__ == "__main__":
