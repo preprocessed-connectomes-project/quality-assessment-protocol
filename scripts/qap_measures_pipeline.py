3dbeaef2c2ae0b2d8cbd0d1bea75c5a206817f58
sgiavasis <sgiava77@gmail.com>
2017-06-13 17:40:01 -0400

    Updated qap_measures_pipeline script to call updated CLI.

--- scripts/qap_measures_pipeline.py
+++ scripts/qap_measures_pipeline.py
@@ -3,8 +3,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-from qap.cli import QAProtocolCLI
+from qap import cli
 
 if __name__ == "__main__":
-    obj = QAProtocolCLI()
-    obj.run()
+    cli.main()
