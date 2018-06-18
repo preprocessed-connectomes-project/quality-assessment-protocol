ac702d0ed7f02544973db00e7cc1bf506bae5f36
anibalsolon <anibalsolon@gmail.com>
2018-06-18 11:31:29 -0400

    qap report types

--- scripts/qap_jsons_to_csv.py
+++ scripts/qap_jsons_to_csv.py
@@ -5,8 +5,7 @@ def main():
 
     import os
     import argparse
-    from qap.script_utils import gather_json_info, json_to_csv
-    from qap.qap_utils import raise_smart_exception
+    from qap.script_utils import gather_json_info, json_to_csv, qap_types
 
     parser = argparse.ArgumentParser()
     parser.add_argument("output_dir", type=str,
@@ -26,22 +25,15 @@ def main():
 
     if args.with_group_reports or args.with_full_reports:
         from qap.viz.reports import workflow_report
-        # TODO: read in combined results dictionary from log JSONs
-        #logs_dict = gather_json_info("_".join([args.output_dir, "logs"]))
-
-        qap_types = ["anatomical_spatial",
-                     "functional_spatial",
-                     "functional_temporal"]
                      
         for qap_type in qap_types:
-            qap_type = "_".join(["qap", qap_type])
-            in_csv = os.path.join(os.getcwd(), '%s.csv' % qap_type)
+            in_csv = os.path.join(os.getcwd(), "qap_%s.csv" % qap_type)
             if not os.path.isfile(in_csv):
                 continue
 
-            reports = workflow_report(in_csv, qap_type, 
-                                      out_dir=args.output_dir,
-                                      full_reports=args.with_full_reports)
+            workflow_report(in_csv, qap_type, 
+                            out_dir=args.output_dir,
+                            full_reports=args.with_full_reports)
 
 
 if __name__ == "__main__":
