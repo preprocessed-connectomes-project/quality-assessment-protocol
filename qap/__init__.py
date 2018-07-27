5afe020827200b39830e66d3afda0c965a004223
anibalsolon <anibalsolon@gmail.com>
2018-07-27 12:02:45 -0400

    import modules to use on the pipeline

--- qap/__init__.py
+++ qap/__init__.py
@@ -1,10 +1,14 @@
+import qap.dvars
+import qap.script_utils
+import qap.bids_utils
+import qap.cloud_utils
 
-from .version import __version__
+from qap.version import __version__
 
-from .anatomical_preproc import run_anatomical_reorient, \
+from qap.anatomical_preproc import run_anatomical_reorient, \
                                run_anatomical_skullstrip, \
                                run_afni_segmentation
 
-from .functional_preproc import run_func_motion_correct, \
+from qap.functional_preproc import run_func_motion_correct, \
                                run_functional_brain_mask, \
                                run_mean_functional
