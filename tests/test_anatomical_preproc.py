f2f93fd7c341d0230852f9c024de536949716491
anibalsolon <anibalsolon@gmail.com>
2018-08-12 18:47:05 -0400

    formatting, linear plugin for wf

--- tests/test_anatomical_preproc.py
+++ tests/test_anatomical_preproc.py
@@ -15,16 +15,26 @@ class TestAnatomicalPreproc(unittest.TestCase):
         self.local_dir = os.path.dirname(os.path.abspath(__file__))
         self.working_dir = os.path.join(self.local_dir, 'workflow_output')
 
-        self.anatomical_scan = os.path.join(self.local_dir, "test_data/anatomical_scan.nii.gz")
-        self.anat_brain = os.path.join(self.local_dir, "test_data/anat_brain.nii.gz")
-        self.anat_reorient = os.path.join(self.local_dir, "test_data/anat_reorient.nii.gz")
+        self.anatomical_scan = os.path.join(
+            self.local_dir, "test_data/anatomical_scan.nii.gz")
+        self.anat_brain = os.path.join(
+            self.local_dir, "test_data/anat_brain.nii.gz")
+        self.anat_reorient = os.path.join(
+            self.local_dir, "test_data/anat_reorient.nii.gz")
 
-        self.config = {"skull_on_registration": True,
-                       "anatomical_template": os.path.join(self.local_dir, "test_data/MNI152_T1_3mm.nii.gz")}
+        self.config = {
+            "skull_on_registration": True,
+            "anatomical_template": os.path.join(self.local_dir, "test_data/MNI152_T1_3mm.nii.gz"),
+            "execution": {
+                "plugin": "Linear",
+                "plugin_args": {},
+                "working_dir": self.working_dir
+            }
+        }
 
         # remove any remnants of previous tests
-        # if os.path.exists(self.working_dir):
-        #     shutil.rmtree(self.working_dir)
+        if os.path.exists(self.working_dir):
+            shutil.rmtree(self.working_dir)
 
     def test_anatomical_reorient(self):
         """
@@ -50,7 +60,8 @@ class TestAnatomicalPreproc(unittest.TestCase):
         resource_pool = {'anat_brain': self.anat_brain}
         out_paths = build_and_run_workflow(anatomical_preproc.afni_segmentation_workflow,
                                            resource_pool, 'afni_segmentation',
-                                           ['anat_csf_mask', 'anat_gm_mask', 'anat_wm_mask'],
+                                           ['anat_csf_mask', 'anat_gm_mask',
+                                               'anat_wm_mask'],
                                            config=self.config)
 
         assert(out_paths != [] and len(out_paths) == 3)
@@ -64,7 +75,8 @@ class TestAnatomicalPreproc(unittest.TestCase):
         resource_pool = {'anatomical_scan': self.anatomical_scan}
         out_paths = build_and_run_workflow(anatomical_preproc.afni_segmentation_workflow,
                                            resource_pool, 'afni_segmentation',
-                                           ['anat_csf_mask', 'anat_gm_mask', 'anat_wm_mask'],
+                                           ['anat_csf_mask', 'anat_gm_mask',
+                                               'anat_wm_mask'],
                                            config=self.config)
 
         assert(out_paths != [] and len(out_paths) == 3)
@@ -76,11 +88,13 @@ class TestAnatomicalPreproc(unittest.TestCase):
         resource_pool = {'anat_reorient': self.anat_reorient}
         out_paths = build_and_run_workflow(anatomical_preproc.afni_anatomical_linear_registration,
                                            resource_pool, 'afni_linear_registration',
-                                           ['anat_linear_xfm', 'anat_linear_warped_anat_reorient'],
+                                           ['anat_linear_xfm',
+                                               'anat_linear_warped_anat_reorient'],
                                            config=self.config)
 
         assert(out_paths != [] and len(out_paths) == 2)
-        assert(out_paths[0].endswith(".aff12.1D") and out_paths[1].endswith(".nii.gz"))
+        assert(out_paths[0].endswith(".aff12.1D")
+               and out_paths[1].endswith(".nii.gz"))
 
     def test_anatomical_skullstrip(self):
         """Run the 'anatomical_skullstrip_workflow' function to execute the
@@ -105,7 +119,8 @@ class TestAnatomicalPreproc(unittest.TestCase):
         try:
             out_paths = build_and_run_workflow(anatomical_preproc.afni_segmentation_workflow,
                                                resource_pool, 'afni_segmentation',
-                                               ['anat_csf_mask', 'anat_gm_mask', 'anat_wm_mask'],
+                                               ['anat_csf_mask', 'anat_gm_mask',
+                                                   'anat_wm_mask'],
                                                config=self.config)
 
         except ValueError:
